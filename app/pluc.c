4fed6884bf25f13d56e18d0b9067d582e3261ad8
kraus <olikraus@gmail.com>
2019-07-28 16:15:11 +0200

    pluc lut mapping

--- app/pluc.c
+++ app/pluc.c
@@ -7,14 +7,165 @@
 */
 
 #include <stdio.h>
+#include <stdint.h>
 #include "dcube.h"
 #include "cmdline.h"
+
+/*==================================================*/
+#define PLUC_REGOP_NOT_USED 0
+#define PLUC_REGOP_ASSIGN_OR 1
+#define PLUC_REGOP_AND 2
+#define PLUC_REGOP_NAND 3
+#define PLUC_REGOP_OR 4
+#define PLUC_REGOP_NOR 5
+#define PLUC_REGOP_AND_OR 6
+#define PLUC_REGOP_NAND_OR 7
+
+#define PLUC_BASE_PLU 0
+#define PLUC_BASE_SWM 1
+
+
+struct _pluc_regop_struct
+{
+  int8_t op;			/* 0: not used, 1: assign "or value" */
+					/* 2: "&=" "and_value", 3: "&= ~" "and_value", 4: "|=" "or_value", 5: "|= ~" "or_value" */
+					/* 6: "&=and_value" "|=or_value", 7: "&=~and_value |=or_value */ 
+  int8_t base;			/* not really a base address,but a index for the base address, 0 means not used, 1 means base addr 0x0 */
+  uint32_t idx;			/* index for base */
+  uint32_t and_value;	/* and value */
+  uint32_t or_value;		/* or value */
+  
+};
+
+typedef struct _pluc_regop_struct pluc_regop_t;
+
+#define PLUC_WIRE_REGOP_CNT 2
+struct _pluc_wire_struct
+{
+  const char *from;
+  const char *to;
+  pluc_regop_t regop[PLUC_WIRE_REGOP_CNT];
+  int8_t is_used;
+};
+typedef struct _pluc_wire_struct pluc_wire_t;
+
+
+
 /*==================================================*/
 /* global variables */
 char c_file_name[1024] = "";
+int cmdline_listmap = 0;
 
 pinfo pi, pi2;
 dclist cl_on, cl_dc, cl2_on, cl2_dc;
+
+
+/*==================================================*/
+/* LPC804 routing information */
+/* 
+  Routing is based on the signal/wire name 
+  LUTx_INPy			y input pin y for lut xx
+  LUTx				x lut output
+  FFx				x flip flip output, note that LUT0 output is automatically mapped to FF0
+  
+*/
+#define PLUC_WIRE_STRING(x) #x
+
+#define PLUC_WIRE_LUT_OUT(x)  PLUC_WIRE_STRING(LUT ## x)
+#define PLUC_WIRE_FF_OUT(x)  PLUC_WIRE_STRING(FF ## x)
+#define PLUC_WIRE_LUT_IN(lut, in) PLUC_WIRE_STRING( LUT ## lut ## _INP ## in)
+#define PLUC_WIRE_PLU_IN(in) PLUC_WIRE_STRING( PLUINPUT ## in)
+
+#define PLUC_CONNECT_PLU_IN_LUT_IN(from, lut, in, idx, v) \
+ { PLUC_WIRE_PLU_IN(from), PLUC_WIRE_LUT_IN(lut, in), {{1, 0, idx, 0, v}, {0, 0, 0, 0, 0}}, 0 } ,
+
+#define PLUC_CONNECT_LUT_OUT_LUT_IN(from, lut, in, idx, v) \
+ { PLUC_WIRE_LUT_OUT(from), PLUC_WIRE_LUT_IN(lut, in), {{1, 0, idx, 0, v}, {0, 0, 0, 0, 0}}, 0 } ,
+
+#define PLUC_CONNECT_FF_OUT_LUT_IN(from, lut, in, idx, v) \
+ { PLUC_WIRE_FF_OUT(from), PLUC_WIRE_LUT_IN(lut, in), {{1, 0, idx, 0, v}, {0, 0, 0, 0, 0}}, 0 } ,
+
+#define PLUC_CONNECT_NONE() \
+ { NULL, NULL, {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}, 0 }
+
+/* from LUT, dest-LUT, input of dest-LUT, index, mux value */
+
+#define LPC804_CONNECT_LUT_IN(lut, in, idx) \
+  PLUC_CONNECT_PLU_IN_LUT_IN(0, lut, in, idx, 0) \
+  PLUC_CONNECT_PLU_IN_LUT_IN(1, lut, in, idx, 1) \
+  PLUC_CONNECT_PLU_IN_LUT_IN(2, lut, in, idx, 2) \
+  PLUC_CONNECT_PLU_IN_LUT_IN(3, lut, in, idx, 3) \
+  PLUC_CONNECT_PLU_IN_LUT_IN(4, lut, in, idx, 4) \
+  PLUC_CONNECT_PLU_IN_LUT_IN(5, lut, in, idx, 5) \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(0, lut, in, idx, 6) \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(1, lut, in, idx, 7) \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(2, lut, in, idx, 8)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(3, lut, in, idx, 9)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(4, lut, in, idx, 10)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(5, lut, in, idx, 11)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(6, lut, in, idx, 12)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(7, lut, in, idx, 13)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(8, lut, in, idx, 14)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(9, lut, in, idx, 15)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(10, lut, in, idx, 16)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(11, lut, in, idx, 17)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(12, lut, in, idx, 18)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(13, lut, in, idx, 19)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(14, lut, in, idx, 20)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(15, lut, in, idx, 21)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(16, lut, in, idx, 22)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(17, lut, in, idx, 23)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(18, lut, in, idx, 24)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(19, lut, in, idx, 25)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(20, lut, in, idx, 26)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(21, lut, in, idx, 27)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(22, lut, in, idx, 28)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(23, lut, in, idx, 29)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(24, lut, in, idx, 30)  \
+  PLUC_CONNECT_LUT_OUT_LUT_IN(25, lut, in, idx, 31) \
+  PLUC_CONNECT_FF_OUT_LUT_IN(0, lut, in, idx, 32) \
+  PLUC_CONNECT_FF_OUT_LUT_IN(1, lut, in, idx, 33) \
+  PLUC_CONNECT_FF_OUT_LUT_IN(2, lut, in, idx, 34) \
+  PLUC_CONNECT_FF_OUT_LUT_IN(3, lut, in, idx, 35)
+
+/* dest-LUT, input of dest-LUT, index input mux */
+#define LPC804_CONNECT_LUT(lut) \
+  LPC804_CONNECT_LUT_IN(lut, 0, lut*0x020+0x000) \
+  LPC804_CONNECT_LUT_IN(lut, 1, lut*0x020+0x004) \
+  LPC804_CONNECT_LUT_IN(lut, 2, lut*0x020+0x008) \
+  LPC804_CONNECT_LUT_IN(lut, 3, lut*0x020+0x00c) \
+  LPC804_CONNECT_LUT_IN(lut, 4, lut*0x020+0x010)
+
+pluc_wire_t lpc804_wire_table[] = {
+  LPC804_CONNECT_LUT(0)
+  LPC804_CONNECT_LUT(1)
+  LPC804_CONNECT_LUT(2)
+  LPC804_CONNECT_LUT(3)
+  LPC804_CONNECT_LUT(4)
+  LPC804_CONNECT_LUT(5)
+  LPC804_CONNECT_LUT(6)
+  LPC804_CONNECT_LUT(7)
+  LPC804_CONNECT_LUT(8)
+  LPC804_CONNECT_LUT(9)
+  LPC804_CONNECT_LUT(10)
+  LPC804_CONNECT_LUT(11)
+  LPC804_CONNECT_LUT(12)
+  LPC804_CONNECT_LUT(13)
+  LPC804_CONNECT_LUT(14)
+  LPC804_CONNECT_LUT(15)
+  LPC804_CONNECT_LUT(16)
+  LPC804_CONNECT_LUT(17)
+  LPC804_CONNECT_LUT(18)
+  LPC804_CONNECT_LUT(19)
+  LPC804_CONNECT_LUT(20)
+  LPC804_CONNECT_LUT(21)
+  LPC804_CONNECT_LUT(22)
+  LPC804_CONNECT_LUT(23)
+  LPC804_CONNECT_LUT(24)
+  LPC804_CONNECT_LUT(25)
+  PLUC_CONNECT_NONE()
+};
+
 
 /*==================================================*/
 void pluc_log(const char *format, ...)
@@ -63,6 +214,8 @@ int pluc_read(void)
       pluc_err("Import pluc_error with file %s", cl_file_list[i]);
       return 0;
     }
+    
+    /* combine both functions, consider the case where the output of cl2 is input of cl */
     pinfoMerge(&pi, cl_on, &pi2, cl2_on);
     
   }
@@ -240,7 +393,7 @@ int pluc(void)
 cl_entry_struct cl_list[] =
 {
   { CL_TYP_STRING,  "oc-write C code", c_file_name, 1024 },
-  //{ CL_TYP_ON,      "greedy-use heuristic cover algorithm", &greedy,  0 },
+  { CL_TYP_ON,      "listmap-list wire mapping", &cmdline_listmap,  0 },
   CL_ENTRY_LAST
 };
 
@@ -261,6 +414,17 @@ int main(int argc, char **argv)
   {
     puts("cmdline pluc_error");
     exit(1);
+  }
+  
+  if ( cmdline_listmap != 0 )
+  {
+    int i = 0;
+    while( lpc804_wire_table[i].from != NULL )
+    {
+      printf("%05d: %-10s -> %-10s\n", i, lpc804_wire_table[i].from, lpc804_wire_table[i].to );
+      i++;
+    }
+    exit(0);
   }
 
   if ( cl_file_cnt < 1 )
