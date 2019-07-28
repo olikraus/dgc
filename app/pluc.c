9dbc498886b3c8e2aef7619d86ce4d80f41fc976
kraus <olikraus@gmail.com>
2019-07-28 23:28:32 +0200

    mapping

--- app/pluc.c
+++ app/pluc.c
@@ -30,7 +30,9 @@ struct _pluc_regop_struct
   int8_t op;			/* 0: not used, 1: assign "or value" */
 					/* 2: "&=" "and_value", 3: "&= ~" "and_value", 4: "|=" "or_value", 5: "|= ~" "or_value" */
 					/* 6: "&=and_value" "|=or_value", 7: "&=~and_value |=or_value */ 
-  int8_t base;			/* not really a base address,but a index for the base address, 0 means not used, 1 means base addr 0x0 */
+  int8_t base;			/* not really a base address,but a index for the base address, 0: PLU, 1: SWM  */
+					/* 0: 0x40028000 PLU   */
+					/* 1: 0x4000c000 SWD  */
   uint32_t idx;			/* index for base */
   uint32_t and_value;	/* and value */
   uint32_t or_value;		/* or value */
@@ -75,15 +77,32 @@ dclist cl_on, cl_dc, cl2_on, cl2_dc;
 #define PLUC_WIRE_FF_OUT(x)  PLUC_WIRE_STRING(FF ## x)
 #define PLUC_WIRE_LUT_IN(lut, in) PLUC_WIRE_STRING( LUT ## lut ## _INP ## in)
 #define PLUC_WIRE_PLU_IN(in) PLUC_WIRE_STRING( PLUINPUT ## in)
+#define PLUC_WIRE_PLU_OUT(out) PLUC_WIRE_STRING( PLUOUT ## out)
 
 #define PLUC_CONNECT_PLU_IN_LUT_IN(from, lut, in, idx, v) \
  { PLUC_WIRE_PLU_IN(from), PLUC_WIRE_LUT_IN(lut, in), {{1, 0, idx, 0, v}, {0, 0, 0, 0, 0}}, 0 } ,
 
+#define LPC804_CONNECT_PLU_OUT_GPIO(out, gpio, o) \
+ { PLUC_WIRE_PLU_OUT(out), #gpio, {{7, 1, 0x180, 3<<(out*2+12), o<<(out*2+12)}, {0, 0, 0, 0, 0}}, 0 } ,
+
 #define PLUC_CONNECT_LUT_OUT_LUT_IN(from, lut, in, idx, v) \
  { PLUC_WIRE_LUT_OUT(from), PLUC_WIRE_LUT_IN(lut, in), {{1, 0, idx, 0, v}, {0, 0, 0, 0, 0}}, 0 } ,
 
+#define LPC804_CONNECT_LUT_OUT_PLU_OUT(lutout, pluout) \
+ { PLUC_WIRE_LUT_OUT(lutout), PLUC_WIRE_PLU_OUT(pluout), {{1, 0, 0xc00+pluout*4, 0, lutout}, {0, 0, 0, 0, 0}}, 0 } ,
+
 #define PLUC_CONNECT_FF_OUT_LUT_IN(from, lut, in, idx, v) \
  { PLUC_WIRE_FF_OUT(from), PLUC_WIRE_LUT_IN(lut, in), {{1, 0, idx, 0, v}, {0, 0, 0, 0, 0}}, 0 } ,
+
+#define LPC804_CONNECT_FF_OUT_PLU_OUT(ffout, pluout) \
+ { PLUC_WIRE_FF_OUT(ffout), PLUC_WIRE_PLU_OUT(pluout), {{1, 0, 0xc00+pluout*4, 0, ffout+26}, {0, 0, 0, 0, 0}}, 0 } ,
+
+#define LPC804_CONNECT_GPIO_PLU_IN(gpio, in, o) \
+ { #gpio, PLUC_WIRE_PLU_IN(in), {{7, 1, 0x180, 3<<(in*2), o<<(in*2)}, {0, 0, 0, 0, 0}}, 0 } ,
+
+#define LPC804_CONNECT_LUT_OUT_FF_OUT(lut, ff) \
+ { PLUC_WIRE_LUT_OUT(lut), PLUC_WIRE_FF_OUT(ff), {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}, 0 } ,
+
 
 #define PLUC_CONNECT_NONE() \
  { NULL, NULL, {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}, 0 }
@@ -134,9 +153,96 @@ dclist cl_on, cl_dc, cl2_on, cl2_dc;
   LPC804_CONNECT_LUT_IN(lut, 1, lut*0x020+0x004) \
   LPC804_CONNECT_LUT_IN(lut, 2, lut*0x020+0x008) \
   LPC804_CONNECT_LUT_IN(lut, 3, lut*0x020+0x00c) \
-  LPC804_CONNECT_LUT_IN(lut, 4, lut*0x020+0x010)
+  LPC804_CONNECT_LUT_IN(lut, 4, lut*0x020+0x010) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 0) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 1) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 2) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 3) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 4) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 5) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 6) \
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(lut, 7)
+
+
+#define LPC804_CONNECT_FF(ff) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 0) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 1) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 2) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 3) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 4) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 5) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 6) \
+  LPC804_CONNECT_FF_OUT_PLU_OUT(ff, 7) \
+  LPC804_CONNECT_LUT_OUT_FF_OUT(ff, ff)
 
 pluc_wire_t lpc804_wire_table[] = {
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_00, 0, 0)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_08, 0, 1)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_17, 0, 2)
+  
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_01, 1, 0)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_09, 1, 1)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_18, 1, 2)
+  
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_02, 2, 0)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_10, 2, 1)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_19, 2, 2)
+  
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_03, 3, 0)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_11, 3, 1)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_20, 3, 2)
+  
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_04, 4, 0)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_12, 4, 1)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_21, 4, 2)
+  
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_05, 5, 0)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_13, 5, 1)
+  LPC804_CONNECT_GPIO_PLU_IN(PIO0_22, 5, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(0, PIO0_07, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(0, PIO0_14, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(0, PIO0_23, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(1, PIO0_08, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(1, PIO0_15, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(1, PIO0_24, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(2, PIO0_09, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(2, PIO0_16, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(2, PIO0_25, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(3, PIO0_10, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(3, PIO0_17, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(3, PIO0_26, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(4, PIO0_11, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(4, PIO0_18, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(4, PIO0_27, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(5, PIO0_12, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(5, PIO0_19, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(5, PIO0_28, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(6, PIO0_13, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(6, PIO0_20, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(6, PIO0_29, 2)
+
+  LPC804_CONNECT_PLU_OUT_GPIO(7, PIO0_14, 0)
+  LPC804_CONNECT_PLU_OUT_GPIO(7, PIO0_21, 1)
+  LPC804_CONNECT_PLU_OUT_GPIO(7, PIO0_30, 2)
+
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(0, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(1, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(2, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(3, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(4, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(5, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(6, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(7, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(8, 0)
+  LPC804_CONNECT_LUT_OUT_PLU_OUT(10, 0)
+
   LPC804_CONNECT_LUT(0)
   LPC804_CONNECT_LUT(1)
   LPC804_CONNECT_LUT(2)
@@ -163,6 +269,12 @@ pluc_wire_t lpc804_wire_table[] = {
   LPC804_CONNECT_LUT(23)
   LPC804_CONNECT_LUT(24)
   LPC804_CONNECT_LUT(25)
+  
+  LPC804_CONNECT_FF(0)
+  LPC804_CONNECT_FF(1)
+  LPC804_CONNECT_FF(2)
+  LPC804_CONNECT_FF(3)
+  
   PLUC_CONNECT_NONE()
 };
 
